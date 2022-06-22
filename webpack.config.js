65cc0d2a784a62c3ce9b1f5fb40ef20605f0a15b
Emryshuang <huanggan3mf@gmail.com>
2022-06-22 21:14:19 +0800

    "babel and eslint config"

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -10,11 +11,47 @@ module.exports = {
         // clean: true
     },
     module: {
-        rules: [{}],
+        rules: [{
+            oneOf: [{
+                test: /\.js$/,
+                // exclude: /node_modules/, // 排除node_modules代码不编译
+                include: path.resolve(__dirname, "./src"), // 也可以用包含
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            cacheDirectory: true, // 开启babel编译缓存
+                            cacheCompression: false, // 缓存文件不要压缩
+                            plugins: ["@babel/plugin-transform-runtime"], // 减少代码体积
+                        },
+                    },
+                ],
+            },]
+        }],
     },
     plugins: [
-        new htmlWebpackPlugin({ template: path.resolve(__dirname, "./public/index.html") }),
+        new ESLintWebpackPlugin({
+            // 指定检查文件的根目录
+            context: path.resolve(__dirname, "./src"),
+            exclude: "node_modules", // 默认值
+            cache: true, // 开启缓存
+            // 缓存目录
+            cacheLocation: path.resolve(
+                __dirname,
+                "./node_modules/.cache/.eslintcache"
+            ),
+        }),
+        new htmlWebpackPlugin({ template: path.resolve(__dirname, "./public/index.html") })
+
     ],
+    resolve: {
+        // 自动补全文件扩展名
+        extensions: [".js", ".json"],
+        // 路径别名
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     devServer: {
         host: "localhost", // 启动服务器域名
         port: "3000", // 启动服务器端口号
