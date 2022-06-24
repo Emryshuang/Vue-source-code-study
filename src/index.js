bc956e32a0204c1e952f1e24a24cd586b1f908bc
Emryshuang <huanggan3mf@gmail.com>
2022-06-24 22:14:27 +0800

    "the diff update function finished without the four condition"

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
 import h from './mysnabbdom/h'
 import patch from './mysnabbdom/patch'
 
-const myVnode1 = h('section', {}, '怪蜀黍的啊B!!!!')
+// const myVnode1 = h('section', {}, '怪蜀黍的啊B!!!!')
 // console.log(myVnode1)
-const myVnode2 = h('section', {}, [
-    h('p', {}, "文字1"),
-    h('p', {}, "文字2"),
-    h('p', {}, "文字3"),
-    h('p', {}, [
-        h('span', {}, '文字4.1'),
-        h('span', {}, '文字4.2')
-    ]),
-    h('p', {},
-        h('span', {}, 'span文字5')
-    ),
-])
-// console.log(myVnode2)
-// const myVnode3 = h('section', {}, [
+// const myVnode2 = h('section', {}, [
 //     h('p', {}, "文字1"),
 //     h('p', {}, "文字2"),
 //     h('p', {}, "文字3"),
-//     h('p', {}, "文字4"),
-//     h('p', {}, '文字5')
+//     h('p', {}, [
+//         h('span', {}, '文字4.1'),
+//         h('span', {}, '文字4.2')
+//     ]),
+//     h('p', {},
+//         h('span', {}, 'span文字5')
+//     ),
 // ])
+// console.log(myVnode2)
+const myVnode3 = h('section', {}, [
+    h('p', { key: '1' }, "文字1"),
+    h('p', { key: '2' }, "文字2"),
+    h('p', { key: '3' }, "文字3"),
+    h('p', { key: '4' }, "文字4"),
+    h('p', { key: '5' }, '文字5'),
+])
 // const myVnode4 = h('p', {}, 'Hi')
+const myVnode5 = h('section', {}, [
+    h('p', { key: '1' }, "文字1"),
+    h('p', { key: '2' }, "文字2"),
+    h('p', { key: '6' }, '文字6'),
+    h('p', { key: '7' }, '文字7'),
+    h('p', { key: '3' }, "文字3"),
+    h('p', { key: '4' }, "文字4"),
+    h('p', { key: '5' }, '文字5'),
+   
+
+])
 const btn = document.querySelector('button')
 const container = document.getElementById('container')
-patch(container, myVnode2)
-btn.onclick = function(){
-    patch(myVnode2, myVnode1)
+patch(container, myVnode5)
+btn.onclick = function () {
+    patch(myVnode5, myVnode3)
 }
 
--- src/mysnabbdom/patch.js
+++ src/mysnabbdom/patch.js
@@ -30,6 +30,91 @@ function createElement(vnode) {
 
   return vnode.elm
 }
+
+function updateChildren(parentElm, oldChildren, newChildren) {
+  console.log('this is updateChildren function', oldChildren, newChildren)
+  let oldStartIdx = 0,
+    oldEndIdx = oldChildren.length - 1,
+    newStartIdx = 0,
+    newEndIdx = newChildren.length - 1
+  let oldStartVnode = oldChildren[oldStartIdx],
+    oldEndVnode = oldChildren[oldEndIdx],
+    newStartVnode = newChildren[newStartIdx],
+    newEndVnode = newChildren[newEndIdx]
+  while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+    console.log("@")
+    if (isSameVnode(oldStartVnode, newStartVnode)) {
+      console.log('the old start and new start')
+      patchVNode(oldStartVnode, newStartVnode)
+      oldStartVnode = oldChildren[++oldStartIdx]
+      newStartVnode = newChildren[++newStartIdx]
+    } else if (isSameVnode(oldEndVnode, newEndVnode)) {
+      console.log('the old end and new end')
+      patchVNode(oldEndVnode, newEndVnode)
+      oldEndVnode = oldChildren[--oldEndIdx]
+      newEndVnode = newChildren[--newEndIdx]
+    } else if (isSameVnode(oldStartVnode, newEndVnode)) {
+      console.log('the old start and new end')
+      patchVNode(oldStartVnode, newEndVnode)
+      parentElm.insertBefore(oldStartVnode.elm, oldEndVnode.elm.nextSibling)
+      oldStartVnode = oldChildren[++oldStartIdx]
+      newEndVnode = newChildren[--newEndIdx]
+    } else if (isSameVnode(oldEndVnode, newStartVnode)) {
+      console.log('the old start and new end')
+      patchVNode(oldEndVnode, newStartVnode)
+      parentElm.insertBefore(oldEndVnode.elm, oldStartVnode.elm)
+      oldEndVnode = oldChildren[--oldEndIdx]
+      newStartVnode = newChildren[++newStartIdx]
+    }
+  }
+
+  if (oldStartIdx <= oldEndIdx) {
+    console.log('old Array has some node to be delete')
+    while(oldStartIdx <= oldEndIdx){
+      parentElm.removeChild(oldChildren[oldStartIdx++].elm)
+    }
+  }
+  
+  if (newStartIdx <= newEndIdx) {    
+    console.log('new Array has some node to be create')
+    const before = newChildren[newEndIdx + 1] == null ? null : newChildren[newEndIdx + 1].elm    
+    while (newStartIdx <= newEndIdx) {
+      parentElm.insertBefore(createElement(newChildren[newStartIdx]), before)
+      newStartIdx++
+    }
+  }
+}
+
+function patchVNode(oldVnode, newVnode) {
+  console.log('the old one and the new one is same node')
+  // is the old one and the new one same obj(same memory address)
+  if (isSameObj(oldVnode, newVnode)) {
+    return
+  }
+  // if (newVnode.text != undefined && !(Array.isArray(newVnode.children) && newVnode.children.length > 0)) 
+  if (newVnode.text != undefined && !(isVnodeHasChildren(newVnode))) {
+    console.log('newVnode contains text and does not contain children')
+    if (oldVnode.text != newVnode.text) {
+      console.log('newVnode text is diffident as oldVnode text')
+      oldVnode.elm.innerText = newVnode.text
+    }
+  } else {
+    console.log('newVnode does not contain text and contains children')
+    if (isVnodeHasChildren(oldVnode)) {
+      console.log('oldVnode contains children')
+      updateChildren(oldVnode.elm, oldVnode.children, newVnode.children)
+    } else {
+      console.log('oldVnode does not contain children')
+      oldVnode.elm.innerHTML = ''
+      for (const child of newVnode.children) {
+        const dom = createElement(child)
+        oldVnode.elm.appendChild(dom)
+      }
+    }
+  }
+  newVnode.elm = oldVnode.elm
+}
+
 export default function patch(oldVnode, newVnode) {
   // the oldVnode is DOM node or Virtual node
   if (oldVnode.sel == '' || oldVnode.sel == undefined) {
@@ -38,32 +123,7 @@ export default function patch(oldVnode, newVnode) {
   console.log('@patch file', oldVnode)
   // is the old one and the new one same node
   if (isSameVnode(oldVnode, newVnode)) {
-    console.log('the old one and the new one is same node')
-    // is the old one and the new one same obj(same memory address)
-    if (isSameObj(oldVnode, newVnode)) {
-      return
-    }
-    // if (newVnode.text != undefined && !(Array.isArray(newVnode.children) && newVnode.children.length > 0)) 
-    if (newVnode.text != undefined && !(isVnodeHasChildren(newVnode))) {
-      console.log('newVnode contains text and does not contain children')
-      if (oldVnode.text != newVnode.text) {
-        console.log('newVnode text is diffident as oldVnode text')
-        oldVnode.elm.innerText = newVnode.text
-      }
-    } else {
-      console.log('newVnode does not contain text and contains children')
-      if (isVnodeHasChildren(oldVnode)) {
-        console.log('oldVnode contains children')
-      } else {
-        console.log('oldVnode does not contain children')
-        oldVnode.elm.innerHTML = ''
-        for (const child of newVnode.children) {
-          const dom = createElement(child)
-          oldVnode.elm.appendChild(dom)
-        }
-      }
-    }
-
+    patchVNode(oldVnode, newVnode)
   } else {
     console.log('the old one and the new one is not same node')
     const newVnodeElmDom = createElement(newVnode)
