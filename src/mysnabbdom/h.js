22fe973f84427e98080ab48215067936b4a64525
Emryshuang <huanggan3mf@gmail.com>
2022-06-25 16:40:09 +0800

    "fix some error word"

--- src/mysnabbdom/h.js
+++ src/mysnabbdom/h.js
@@ -1,15 +1,15 @@
 import vnode from './vnode'
-// console.log(vnode)
+
 
 export default function (sel, data, c) {
     if (arguments.length != 3) {
-        throw new Error('h function need 3 arguments in this edtion')
+        throw new Error('h function need 3 arguments in this edition')
     }
     if (typeof c == 'string' || typeof c == 'number') {
-        console.log('edtion 1')
+        console.log('edition 1')
         return vnode(sel, data, undefined, c, undefined)
     } else if (Array.isArray(c)) {
-        console.log('edtion 2')
+        console.log('edition 2')
         const children = []
         for (let i = 0; i < c.length; ++i) {
             if (!(typeof c[i] == 'object' && Object.prototype.hasOwnProperty.call(c[i], 'sel'))) {
@@ -19,9 +19,9 @@ export default function (sel, data, c) {
         }
         return vnode(sel, data, children, undefined, undefined)
     } else if (typeof c == 'object' && Object.prototype.hasOwnProperty.call(c, 'sel')) {
-        console.log('edtion 3')
+        console.log('edition 3')
         return vnode(sel, data, [c], undefined, undefined)
     } else {
-        console.log('edtion wrong')
+        throw new Error('h function need 3 arguments are selection, data, children or text')
     }
 }
